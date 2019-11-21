887bbf9a679f420594f96f3302e24a3f4de6165d
tyler <tdriver@talentpath.com>
2019-11-21 11:06:06 -0600

    Adding stuff for Cors and creating test files

--- SQL/api.js
+++ SQL/api.js
@@ -3,6 +3,7 @@ const port = 8080;
 
 http
   .createServer((req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
     res.end();
   })
   .listen(port, () => console.log("Listening on localhost:" + port));
